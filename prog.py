e58660d81392d19604b5f162aadd7c1fa45bbde9
willsc <cameron.wills.1998@gmail.com>
2024-05-22 15:53:45 -0600

    Added mutually exclusive group

--- prog.py
+++ prog.py
@@ -1,13 +1,17 @@
 import argparse
+
 parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
 parser.add_argument("x", type=int, help="the base")
 parser.add_argument("y", type=int, help="the exponent")
-parser.add_argument("-v", "--verbosity", action="count", default=0)
 args = parser.parse_args()
 answer = args.x**args.y
-if args.verbosity >= 2:
-    print(f"{args.x} to the power {args.y} equals {answer}")
-elif args.verbosity >= 1:
-    print(f"{args.x}^{args.y} == {answer}")
-else:
+
+if args.quiet:
     print(answer)
+elif args.verbose:
+    print(f"{args.x} to the power {args.y} equals {answer}")
+else:
+    print(f"{args.x}^{args.y} == {answer}")
