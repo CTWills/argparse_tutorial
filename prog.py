278698bab9c0c7624af6dfafc126d45b7b9477df
willsc <cameron.wills.1998@gmail.com>
2024-05-22 15:55:33 -0600

    Added description for program

--- prog.py
+++ prog.py
@@ -1,6 +1,6 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="calculate X to the power of Y")
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-v", "--verbose", action="store_true")
 group.add_argument("-q", "--quiet", action="store_true")
