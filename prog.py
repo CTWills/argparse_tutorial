3f2c31db9d1ae1fe28ecb2992bd860dc45520048
willsc <cameron.wills.1998@gmail.com>
2024-05-22 15:37:18 -0600

    Added sqaure and verbose options

--- prog.py
+++ prog.py
@@ -1,8 +1,13 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
 parser.add_argument("-v", "--verbose", help="increase output verbosity",
                     action="store_true")
 args = parser.parse_args()
+answer = args.square**2
 if args.verbose:
-    print("Verbosity turned on")
+    print(f"the square of {args.square} equals {answer}")
+else:
+    print(answer)
